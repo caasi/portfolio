d46f92922325857cdd2755dd007bad463a47ec65
caasi Huang <caasi.igd@gmail.com>
2014-03-07 00:19:05 +0800

    auto resize the stage

--- main.js
+++ main.js
@@ -14,4 +14,59 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  **/
 
-console.log('foo');
+var $win, config, dimension, computePosScale, stage, dim, setting, x$, gameStage, renderer, animate;
+$win = $(window);
+config = {
+  width: 320,
+  height: 240
+};
+dimension = function(){
+  return {
+    w: $win.width(),
+    h: $win.height()
+  };
+};
+computePosScale = function(dim){
+  var ratio, s;
+  ratio = {
+    w: dim.w / config.width,
+    h: dim.h / config.height
+  };
+  s = Math.min(ratio.w, ratio.h);
+  return {
+    scale: {
+      x: s,
+      y: s
+    },
+    offset: {
+      x: (dim.w - config.width * s) / 2,
+      y: (dim.h - config.height * s) / 2
+    }
+  };
+};
+stage = new PIXI.Stage(0x000000);
+dim = dimension();
+setting = computePosScale(dim);
+x$ = gameStage = new PIXI.Graphics;
+x$.beginFill(0xff9900);
+x$.drawRect(0, 0, config.width, config.height);
+x$.endFill();
+x$.position = setting.offset;
+x$.scale = setting.scale;
+stage.addChild(gameStage);
+renderer = PIXI.autoDetectRenderer($win.width(), $win.height());
+$('body').append(renderer.view);
+$(window).resize(function(){
+  var dim, setting, x$;
+  dim = dimension();
+  setting = computePosScale(dim);
+  renderer.resize(dim.w, dim.h);
+  x$ = gameStage;
+  x$.position = setting.offset;
+  x$.scale = setting.scale;
+});
+animate = function(){
+  requestAnimationFrame(animate);
+  renderer.render(stage);
+};
+requestAnimationFrame(animate);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,1 +1,49 @@
-console.log \foo
+$win   = $ window
+config =
+  width:  320
+  height: 240
+
+dimension = ->
+  w: $win.width!
+  h: $win.height!
+
+compute-pos-scale = (dim) ->
+  ratio =
+    w: dim.w / config.width
+    h: dim.h / config.height
+  s = Math.min ratio.w, ratio.h
+  scale:
+    x: s
+    y: s
+  offset:
+    x: (dim.w - config.width  * s) / 2
+    y: (dim.h - config.height * s) / 2
+
+stage = new PIXI.Stage 0x000000
+
+dim = dimension!
+setting = compute-pos-scale dim
+game-stage = new PIXI.Graphics
+  ..beginFill 0xff9900
+  ..drawRect 0 0 config.width, config.height
+  ..endFill!
+  ..position = setting.offset
+  ..scale    = setting.scale
+stage.addChild game-stage
+
+renderer = PIXI.autoDetectRenderer $win.width!, $win.height!
+$(\body).append renderer.view
+
+$(window).resize !->
+  dim = dimension!
+  setting = compute-pos-scale dim
+  renderer.resize dim.w, dim.h
+  game-stage
+    ..position = setting.offset
+    ..scale    = setting.scale
+
+animate = !->
+  requestAnimationFrame animate
+  renderer.render stage
+requestAnimationFrame animate
+
